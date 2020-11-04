f1f40f6ab499903455ff41be0304b76f43f58d27
tutajm <mtutaj@mcw.edu>
2020-11-04 16:56:07 -0600

    added loader for study 3101

---
+++ src/main/java/edu/mcw/rgd/phenominer/ImportCommon.java
@@ -0,0 +1,166 @@
+package edu.mcw.rgd.phenominer;
+
+import edu.mcw.rgd.dao.impl.PhenominerDAO;
+import edu.mcw.rgd.datamodel.pheno.*;
+import edu.mcw.rgd.process.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** common code to be used by specialized loaders */
+
+public class ImportCommon {
+
+    PhenominerDAO pdao = new PhenominerDAO();
+
+    Experiment loadExperiment(int sid, String vtID, String vtName, List<Experiment> experimentsInRgd) throws Exception {
+
+        // find a matching experiment in RGD
+        for( Experiment exp: experimentsInRgd ) {
+            if( exp.getTraitOntId().equals(vtID) ) {
+                return exp;
+            }
+        }
+
+        // not in RGD yet: add to RGD DB and then to the list
+        Experiment exp = new Experiment();
+        exp.setName(vtName);
+        exp.setStudyId(sid);
+        exp.setTraitOntId(vtID);
+        pdao.insertExperiment(exp);
+        experimentsInRgd.add(exp);
+        return exp;
+    }
+
+    Record parseStrainRecord(int expId, String cmoId, String[] cols, int strainCol, String rsId, String units) {
+
+        boolean noStrainInfo = Utils.isStringEmpty( String.join("", cols[strainCol], cols[strainCol+1], cols[strainCol+2], cols[strainCol+3], cols[strainCol+4], cols[strainCol+5], cols[strainCol+6]));
+        if( noStrainInfo ) {
+            return null;
+        }
+
+        Record er = new Record();
+        er.setExperimentId(expId);
+
+        er.setMeasurementUnits(units);
+        er.setMeasurementValue(cols[strainCol]);
+        er.setMeasurementSD(cols[strainCol+1]);
+        er.setMeasurementSem(cols[strainCol+2]);
+
+        Sample s = new Sample();
+        s.setStrainAccId(rsId);
+
+        // handle age range
+        String age = cols[strainCol+4];
+        int dashPos = age.indexOf('-');
+        if( dashPos>0 ) {
+            s.setAgeDaysFromLowBound(Integer.parseInt(age.substring(0, dashPos)));
+            s.setAgeDaysFromHighBound(Integer.parseInt(age.substring(dashPos+1)));
+        } else {
+            s.setAgeDaysFromLowBound(Integer.parseInt(age));
+            s.setAgeDaysFromHighBound(s.getAgeDaysFromLowBound());
+        }
+
+        s.setNumberOfAnimals(Integer.parseInt(cols[strainCol+3]));
+
+        String sex = cols[strainCol+6].toLowerCase().trim();
+        s.setSex(sex);
+
+        er.setSample(s);
+
+        ClinicalMeasurement cm = new ClinicalMeasurement();
+        cm.setAccId(cmoId);
+        er.setClinicalMeasurement(cm);
+
+        MeasurementMethod mm = new MeasurementMethod();
+        mm.setAccId(cols[29]);
+        er.setMeasurementMethod(mm);
+
+        return er;
+    }
+
+    List<Condition> parseConditions(String[] cols, int colsPerConditionSet) {
+
+        List<Condition> conditionList = new ArrayList<Condition>();
+        int xcoCol = 31;
+        for( int ecCol=xcoCol; ; ecCol+=colsPerConditionSet ) {
+            String xcoId = cols[ecCol+1];
+            if( Utils.isStringEmpty(xcoId) ) {
+                break;
+            }
+            Condition cond = new Condition();
+            cond.setOntologyId(xcoId);
+
+            // duration string: '2419200 s', or '12 hrs', or '30min'
+            String sDuration = cols[ecCol+3].trim();
+            if( !Utils.isStringEmpty(sDuration) ) {
+                long multiplier = 0;
+                if (sDuration.endsWith(" s")) {
+                    sDuration = sDuration.substring(0, sDuration.length() - 2).trim();
+                    multiplier = 1;
+                } else if (sDuration.endsWith(" hrs")) {
+                    sDuration = sDuration.substring(0, sDuration.length() - 4).trim();
+                    multiplier = 60 * 60; // hours to seconds
+                } else if (sDuration.endsWith("min")) {
+                    sDuration = sDuration.substring(0, sDuration.length() - 3).trim();
+                    multiplier = 60; // minutes to seconds
+                }
+                long duration = Long.parseLong(sDuration) * multiplier;
+                cond.setDurationLowerBound(duration);
+                cond.setDurationUpperBound(duration);
+            }
+
+            // cond value, f.e. '5 ml/kg'
+            String value = cols[ecCol+2];
+            int spacePos = value.indexOf(' ');
+            if( spacePos>0 ) {
+                cond.setValue(value.substring(0, spacePos));
+                cond.setUnits(value.substring(spacePos+1));
+            }
+            cond.setApplicationMethod(cols[ecCol+4]);
+
+            conditionList.add(cond);
+            cond.setOrdinality(conditionList.size());
+        }
+
+        return conditionList;
+    }
+
+    public void deleteRecord(int recordId) throws Exception {
+        try {
+            pdao.deleteIndividualRecords(recordId);
+        } catch (Exception var9) {
+            var9.printStackTrace();
+        }
+
+        Record rec = pdao.getRecord(recordId);
+
+        try {
+            pdao.deleteExperimentConditions(rec.getConditions());
+        } catch (Exception var5) {
+            var5.printStackTrace();
+        }
+
+        String sql = "delete from experiment_record where experiment_record_id=?";
+        pdao.update(sql, new Object[]{recordId});
+
+        try {
+            pdao.deleteSample(rec.getSampleId());
+        } catch (Exception var8) {
+            var8.printStackTrace();
+        }
+
+        try {
+            pdao.deleteMeasurementMethod(rec.getMeasurementMethodId());
+        } catch (Exception var7) {
+            var7.printStackTrace();
+        }
+
+        try {
+            pdao.deleteClinicalMeasurement(rec.getClinicalMeasurementId());
+        } catch (Exception var6) {
+            var6.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/edu/mcw/rgd/phenominer/ImportStudy3101.java
@@ -0,0 +1,104 @@
+package edu.mcw.rgd.phenominer;
+
+import edu.mcw.rgd.datamodel.pheno.*;
+import edu.mcw.rgd.process.Utils;
+
+import java.io.BufferedReader;
+import java.util.List;
+
+/** load phenominer study from Excel template, Nov 4, 2020
+ *
+ */
+public class ImportStudy3101 extends ImportCommon {
+
+    public static void main(String[] args) throws Exception {
+
+        new ImportStudy3101().run();
+    }
+
+    final int sid = 3101; // STUDY_ID
+    final String[] STRAIN_ONT_IDS = { // in order as they appear in the template
+            "RS:0000015", // SHR
+            "RS:0000742", // WKY
+            "RS:0000967", // WAG
+    };
+
+    void run() throws Exception {
+
+        // delete records for all experiments
+        if( true ) {
+            int recordsDeleted = 0;
+            for (Experiment e : pdao.getExperiments(sid)) {
+                List<Record> records = pdao.getRecords(e.getId());
+                for (Record r : records) {
+                    deleteRecord(r.getId());
+                    recordsDeleted++;
+                }
+            }
+            System.out.println("records deleted: "+recordsDeleted);
+        }
+
+        Study study = pdao.getStudy(sid);
+        List<Experiment> experiments = pdao.getExperiments(sid);
+        int recordsInserted = 0;
+
+        String fname = "src/main/resources/study3101_romanowska.txt";
+        BufferedReader bw = Utils.openReader(fname);
+
+        String line;
+        while( (line=bw.readLine())!=null ) {
+            String[] cols = line.split("[\\t]", -1);
+
+            // vt id must be in column 0
+            String vtId = cols[0];
+            if( !vtId.startsWith("VT:") || vtId.length()!=10 ) {
+                continue;
+            }
+            String vtName = cols[2];
+            String cmoId = cols[3];
+            String units = cols[4];
+
+            Experiment experiment = loadExperiment(sid, vtId, vtName, experiments);
+            System.out.println("EID: "+experiment.getId());
+
+            List<Condition> conditionList = parseConditions(cols, 5);
+
+            // there are three strains, possibly empty; every strain spans 7 columns
+            int strainCol = 5;
+            Record er1 = parseStrainRecord(experiment.getId(), cmoId, cols, strainCol, STRAIN_ONT_IDS[0], units);
+
+            if( er1!=null ) {
+                er1.setConditions(conditionList);
+                pdao.insertRecord(er1);
+                recordsInserted++;
+            }
+
+
+            // strain 2
+            strainCol += 7;
+            Record er2 = parseStrainRecord(experiment.getId(), cmoId, cols, strainCol, STRAIN_ONT_IDS[1], units);
+
+            if( er2!=null ) {
+                er2.setConditions(conditionList);
+                pdao.insertRecord(er2);
+                recordsInserted++;
+            }
+
+
+            // strain 3
+            strainCol += 7;
+            Record er3 = parseStrainRecord(experiment.getId(), cmoId, cols, strainCol, STRAIN_ONT_IDS[2], units);
+
+            if( er3!=null ) {
+                er3.setConditions(conditionList);
+                pdao.insertRecord(er3);
+                recordsInserted++;
+            }
+        }
+
+        bw.close();
+
+        System.out.println("OK -- records inserted "+recordsInserted);
+    }
+
+}
